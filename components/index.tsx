28bf44aa38c41773ebf9ff09c5d85c8f22590fbf
YU <a572487953@gmail.com>
2023-08-04 01:41:02 -0400

    page structure

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+export {
+  Hero,
+  Footer,
+  NavBar,
+  CustomButton
+}
